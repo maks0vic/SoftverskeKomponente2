f105b83c05e83d323da46e2a5a32229ce61c17bd
StefanMaksovic <smaksovic1118rn@raf.rs>
2020-12-15 16:05:42 +0100

    Prvi commit

---
+++ User_servis/src/main/java/sk2/User_servis/UserServisApplication.java
@@ -0,0 +1,13 @@
+package sk2.User_servis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServisApplication.class, args);
+	}
+
+}
