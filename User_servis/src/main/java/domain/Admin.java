1958b89a6f227e844880156cddd8baaddf682288
StefanMaksovic <smaksovic1118rn@raf.rs>
2020-12-15 16:19:49 +0100

    Ubacen domain i dto

---
+++ User_servis/src/main/java/domain/Admin.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Admin {
+	private String username;
+	private String password;
+	
+	public Admin() {
+		super();
+	}
+
+	public Admin(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+}
